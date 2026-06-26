c602f03f3884e321b2aead63ae34f8f05480230f
ItsMeVishal0 <ItsMeVishal0@users.noreply.github.com>
2026-06-27 05:09:14 +0930

    VishalMusic

--- VISHALMUSIC/utils/inline/__init__.py
+++ VISHALMUSIC/utils/inline/__init__.py
@@ -1,7 +1,6 @@
-from .extras import *
+﻿from .extras import *
 from .help import *
 from .play import *
-from .play import colored_stream_markup, colored_stream_markup_timer, colored_control_buttons
 from .queue import *
 from .settings import *
 from .start import *
